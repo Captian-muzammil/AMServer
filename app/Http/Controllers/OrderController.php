997e23fba8edcb080b3d7e00b50ff4681384e41c
Captian-muzammil <muzammilas7k@gmail.com>
2019-02-15 23:56:49 +0530

    add total_order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,6 +15,36 @@ class OrderController extends Controller
         $this->order = $tag;
     }
 
+    public function totalOrder(){
+        /* Default Variables */
+        $fields = []; // list of fields to be fetched (products collection)
+        $filters = [];
+        $limit = Input::has('limit') ? (int) Input::get('limit') : 25; // number of entries
+        $active = true;
+        $with = [];
+        $returnData = null;
+        $sort = ['id'];
+        /* Default Variables */
+
+        /* Set Page */
+        $page = 1;
+        if (Input::has('page')) {
+            $page = Input::get('page');
+        }
+        /* Set Page */
+
+
+        try {
+            /* Get Match Event List */
+            $orders = $this->order->listing($limit, $active, $fields, $filters, $sort, $with,$page);
+            /* Get Match Event List */
+
+            return response()->json(['message' => '', 'data' => count($orders)],200);
+        } catch (Exception $e) {
+            return response()->json(['code' => 500, 'message' => Lang::get('apiStatusCodes.oops')], 500);
+        }
+    }
+
     public function index() {
         /* Default Variables */
         $fields = []; // list of fields to be fetched (products collection)
@@ -44,7 +74,7 @@ class OrderController extends Controller
             }
 
 
-            return response()->json(['message' => '', 'data' => $returnData],200);
+            return response()->json(['message' => 'success', 'data' => $returnData],200);
         } catch (Exception $e) {
             return response()->json(['code' => 500, 'message' => Lang::get('apiStatusCodes.oops')], 500);
         }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/order', 'OrderController@index');
+Route::get('/total_order', 'OrderController@totalOrder');
